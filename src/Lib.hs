197b574fbd58963edf918146e761d878bda5fa5a
Steven MacCoun <theraccoun@gmail.com>
2018-03-24 17:34:46 -0600

    Cors middleware

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,12 +6,15 @@ module Lib
     ) where
 
 import Servant.API
+import Data.Default
 import qualified System.Log.FastLogger as FL
 import qualified Servant                              as S
 import           Control.Natural                      ((:~>) (NT))
 import           Control.Monad.Trans.Reader (runReaderT)
 import Control.Monad.Except (catchError)
 import Data.Aeson
+import           Network.Wai.Middleware.Cors
+import qualified Network.Wai.Middleware.RequestLogger as MidRL
 import qualified Network.Wai                          as Wai
 import qualified Network.Wai.Handler.Warp             as Warp
 
@@ -45,9 +48,12 @@ startApp args = do
       Nothing -> lookupEnvDefault "SERVANT_LOG" STDOut
 
     logger  <- makeLogger logTo
+    midware   <- makeMiddleware logger env
     let initialLogMsg = intercalate " " ["Listening on port", show port, "at level", show env, "and logging to", show logTo, "with args", intercalate "," args,"\n"]
     FL.pushLogStr logger $ FL.toLogStr (toText initialLogMsg )
-    Warp.run 8080 $ app (Config logger)
+    Warp.run port
+      $ midware
+      $ app (Config logger)
 
 app :: App.Config -> Wai.Application
 app config = do
@@ -67,4 +73,28 @@ runHandler config handler =
 
 nt :: App.Config -> AppM a -> S.Handler a
 nt s x = runReaderT x s
+
+makeMiddleware :: FL.LoggerSet -> Environment -> IO Wai.Middleware
+makeMiddleware logger env =
+          combineMiddleware corsified
+        $ MidRL.mkRequestLogger
+        $ def { MidRL.destination = MidRL.Logger logger }
+
+corsified :: Wai.Middleware
+corsified = cors (const $ Just appCorsResourcePolicy)
+
+combineMiddleware :: Wai.Middleware -> IO Wai.Middleware -> IO Wai.Middleware
+combineMiddleware a = fmap (. a)
+
+appCorsResourcePolicy :: CorsResourcePolicy
+appCorsResourcePolicy = CorsResourcePolicy {
+    corsOrigins        = Nothing
+  , corsMethods        = ["OPTIONS", "GET", "PUT", "POST", "PATCH", "DELETE"]
+  , corsRequestHeaders = ["Authorization", "Content-Type"]
+  , corsExposedHeaders = Nothing
+  , corsMaxAge         = Nothing
+  , corsVaryOrigin     = False
+  , corsRequireOrigin  = False
+  , corsIgnoreFailures = False
+}
 
