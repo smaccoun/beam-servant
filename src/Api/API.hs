118dd51af268a867ed9043b580dbd94bb1bd1ed6
Steven MacCoun <theraccoun@gmail.com>
2018-03-30 19:41:24 -0600

    Make current endpoints unprotected

--- src/Api/API.hs
+++ src/Api/API.hs
@@ -6,14 +6,23 @@ import Api.User
 import Servant
 import Data.Text (Text)
 
-type API =
+type Unprotected =
        Get '[JSON] Text
   :<|> UserAPI
+
+unprotectedProxy :: Proxy Unprotected
+unprotectedProxy = Proxy
+
+unprotectedS :: ServerT Unprotected AppM
+unprotectedS =
+       return "hello world"
+  :<|> userServer
+
+
+type API = Unprotected
 
 api :: Proxy API
 api = Proxy
 
 server :: ServerT API AppM
-server =
-       return "hello world"
-  :<|> userServer
+server = unprotectedS
