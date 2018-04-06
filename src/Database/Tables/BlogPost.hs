983d29ee5ebcceb3dc198f6ec87815ffd43f78d7
Steven MacCoun <theraccoun@gmail.com>
2018-04-06 15:15:06 -0600

    Simple blog post model

---
+++ src/Database/Tables/BlogPost.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Database.Tables.BlogPost where
+
+import           AppPrelude
+import           Control.Lens       hiding (element)
+import           Data.UUID          (UUID)
+import           Database.Beam
+import           GHC.Generics       (Generic)
+import           Models.Credentials (Email (..))
+import           Prelude            (String)
+
+type BlogPostID = UUID
+
+data BlogPostT f
+    = BlogPost
+    { _blogPostId :: Columnar f BlogPostID
+    , _title      :: Columnar f Text
+    , _content    :: Columnar f Text
+    } deriving (Generic)
+
+type BlogPost = BlogPostT Identity
+
+makeLenses ''BlogPostT
+
+instance Beamable BlogPostT
+instance Table BlogPostT where
+  data PrimaryKey BlogPostT f = BlogPostID (Columnar f UUID) deriving Generic
+  primaryKey = BlogPostID . _blogPostId
+
+instance Beamable (PrimaryKey UserT)
+deriving instance Show BlogPost
