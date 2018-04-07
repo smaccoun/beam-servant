1020e07200afde697d70fc8a8632187d5e15e44b
Steven MacCoun <theraccoun@gmail.com>
2018-04-07 14:46:20 -0600

    newtype json unwrapping

--- src/Models/Credentials.hs
+++ src/Models/Credentials.hs
@@ -5,16 +5,27 @@
 module Models.Credentials where
 
 import           AppPrelude
-import           Control.Lens
+import           Control.Lens (makeLenses)
 import           Data.Aeson
 import           Data.Text    (Text)
 
 newtype Email =
   Email {_unEmail :: Text}
-    deriving (Generic, Eq, Show, Read, FromJSON, ToJSON)
+    deriving (Generic, Eq, Show, Read)
+
+instance FromJSON Email where
+  parseJSON (String v) = pure (Email v)
+  parseJSON _          = mzero
+
+instance ToJSON Email where
+  toJSON (Email email') = object ["email" .= email']
 
 makeLenses ''Email
 
 newtype Password =
   Password {unPassword :: Text}
-    deriving (Generic, Eq, Show, Read, FromJSON, ToJSON)
+    deriving (Generic, Eq, Show, Read)
+
+instance FromJSON Password where
+  parseJSON (String v) = pure (Password v)
+  parseJSON _          = mzero
