f1a444d4179075cc779645800f95399b39431f20
Steven MacCoun <theraccoun@gmail.com>
2018-05-26 15:56:51 -0600

    Delete resource for full CRUD

--- src/Api/Resource.hs
+++ src/Api/Resource.hs
@@ -5,58 +5,94 @@
 
 module Api.Resource where
 
-import           App
 import           AppPrelude
 import           Pagination
 import           Servant
 
 
-{- Read API -}
-type RResourceAPI (resourceName :: Symbol) c a i = resourceName :>
-  (    QueryParam "limit" Limit
+type GetListAPI c a =
+  QueryParam "limit" Limit
     :> QueryParam "page" Offset
     :> QueryParam "orderBy" Text
     :> Get '[JSON] (c a)
 
-  :<|> Capture "id" i    :> Get '[JSON] a
+type GetItemAPI a i =
+  Capture "id" i    :> Get '[JSON] a
+
+type GetCollectionServer m c a = (Maybe Limit -> Maybe Offset -> Maybe Text -> m (c a))
+
+{- Read API -}
+type RResourceAPI (resourceName :: Symbol) c a i = resourceName :>
+  (    GetListAPI c a
+  :<|> GetItemAPI a i
   )
 
 rResourceServer ::
-     (Maybe Limit -> Maybe Offset -> Maybe Text -> m (c a))
+     GetCollectionServer m c a
   -> (i -> m a)
   -> ServerT (RResourceAPI name c a i) m
 rResourceServer listAs getA =
   listAs :<|> getA
 
+
 {- Create/Read API -}
-type CRResourceAPI (resourceName :: Symbol) a i = resourceName :>
-  (    Get '[JSON] [a]
-  :<|> Capture "id" i    :> Get '[JSON] a
-  :<|> ReqBody '[JSON] a :> Post '[JSON] NoContent
+
+type CreateAPI a =
+   ReqBody '[JSON] a :> Post '[JSON] NoContent
+
+type CRResourceAPI (resourceName :: Symbol) c a i = resourceName :>
+  (    GetListAPI c a
+  :<|> GetItemAPI a i
+  :<|> CreateAPI a
   )
 
 crResourceServer
-  :: AppM [a]
-  -> (i -> AppM a)
-  -> (a -> AppM NoContent)
-  -> ServerT (CRResourceAPI name a i) AppM
+  :: GetCollectionServer m c a
+  -> (i -> m a)
+  -> (a -> m NoContent)
+  -> ServerT (CRResourceAPI name c a i) m
 crResourceServer listAs getA postA =
   listAs :<|> getA :<|> postA
 
 
 {- Create/Read/Update API -}
-type CRUResourceAPI (resourceName :: Symbol) a i = resourceName :>
-  (    Get '[JSON] [a]
-  :<|> Capture "id" i    :> Get '[JSON] a
-  :<|> ReqBody '[JSON] a :> Post '[JSON] NoContent
-  :<|> ReqBody '[JSON] a :> Patch '[JSON] NoContent
+type UpdateAPI a =
+   ReqBody '[JSON] a :> Patch '[JSON] NoContent
+
+type CRUResourceAPI (resourceName :: Symbol) c a i = resourceName :>
+  (    GetListAPI c a
+  :<|> GetItemAPI a i
+  :<|> CreateAPI a
+  :<|> UpdateAPI a
   )
 
 cruResourceServer
-  :: AppM [a]
-  -> (i -> AppM a)
-  -> (a -> AppM NoContent)
-  -> (a -> AppM NoContent)
-  -> ServerT (CRUResourceAPI name a i) AppM
+  :: GetCollectionServer m c a
+  -> (i -> m a)
+  -> (a -> m NoContent)
+  -> (a -> m NoContent)
+  -> ServerT (CRUResourceAPI name c a i) m
 cruResourceServer listAs getA postA updateA =
   listAs :<|> getA :<|> postA :<|> updateA
+
+{- Create/Read/Update/Delete API -}
+type DeleteAPI a i =
+   Capture "id" i :> Delete '[JSON] NoContent
+
+type CRUDResourceAPI (resourceName :: Symbol) c a i = resourceName :>
+  (    GetListAPI c a
+  :<|> GetItemAPI a i
+  :<|> CreateAPI a
+  :<|> UpdateAPI a
+  :<|> DeleteAPI a i
+  )
+
+crudResourceServer
+  :: GetCollectionServer m c a
+  -> (i -> m a)
+  -> (a -> m NoContent)
+  -> (a -> m NoContent)
+  -> (i -> m NoContent)
+  -> ServerT (CRUDResourceAPI name c a i) m
+crudResourceServer listAs getA postA updateA deleteA =
+  listAs :<|> getA :<|> postA :<|> updateA :<|> deleteA
