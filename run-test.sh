252215ad5719a38fa8b3d1635ca1d99379295f84
Steven MacCoun <theraccoun@gmail.com>
2018-05-31 17:39:35 -0600

    Add tests to circle

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-docker stop test-db
+docker stop test-db && docker rm test-db
+
 docker run --name test-db  -p 7543:5432 -h 127.0.0.1 --env-file .env.test -d postgres
 (env $(cat .env.test | xargs) stack test )
